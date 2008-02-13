7a0e4b001bf47b32df1fbc2e55b2852423ff8ff5
justingrevich.com <justingrevich.com@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2008-02-13 00:51:33 +0000

    another days work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,7 +26,7 @@ class SessionsController < ApplicationController
     self.current_user.forget_me if logged_in?
     cookies.delete :auth_token
     reset_session
-    flash[:notice] = "You have been logged out."
+    flash[:notice] = "You have been logged out"
     redirect_back_or_default('/')
   end
 end
