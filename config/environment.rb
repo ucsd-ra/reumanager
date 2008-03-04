0b00fd32917d3db78d7629b91ed607c70005c412
justingrevich.com <justingrevich.com@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2008-03-04 22:44:21 +0000

    working ActionController::AbstractRequest.relative_url_root = nsfreu

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 
 # Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
-ENV['RAILS_ENV'] ||= 'production'
+#ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
@@ -46,7 +46,6 @@ Rails::Initializer.run do |config|
   # which shouldn't be used to store highly confidential information
   # (create the session table with 'rake db:sessions:create')
   config.action_controller.session_store = :active_record_store
-  config.action_controller.abstract_request.relative_url_root = '/test'
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
   # like if you have constraints or database-specific column types
