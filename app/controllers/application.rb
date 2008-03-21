96011413246624a651366bd556143a8a18661d5d
jjg <jjg@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2008-03-21 15:55:33 +0000

    added   filter_parameter_logging :password to prevent passwords leaking into the log

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   include SslRequirement
   include SimpleCaptcha::ControllerHelpers
+  filter_parameter_logging :password
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery :secret => '25d978808669907c9d5aa5c38672540d'
