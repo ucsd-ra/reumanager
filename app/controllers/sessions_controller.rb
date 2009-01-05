f81e0573ad8967a16e8dea4c877819d16f23eab3
jjg <jjg@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2009-01-05 22:07:57 +0000

    fixed admin redirection problem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
         self.current_user = user
         new_cookie_flag = (params[:remember_me] == "1")
         handle_remember_cookie! new_cookie_flag
-        if (user.id == 1 || 6)
+        if (user.email == "jgrevich@gmail.com" || user.email == "mmicou@ucsd.edu")
           redirect_to( :controller => "admin" )
         else
           if user.submit_date
