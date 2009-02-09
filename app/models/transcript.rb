9d4d259b7f184790697a8480721d349c52dd2444
jjg <jjg@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2009-02-09 15:47:10 +0000

    increase size of attachment to 20 megabytes

--- app/models/transcript.rb
+++ app/models/transcript.rb
@@ -1,5 +1,5 @@
 class Transcript < ActiveRecord::Base
   belongs_to                :user
-  has_attachment            :content_type => ["application/pdf", "application/octet-stream"], :storage => :file_system, :max_size => 5.megabytes
+  has_attachment            :content_type => ["application/pdf", "application/octet-stream"], :storage => :file_system, :max_size => 20.megabytes
   validates_as_attachment
 end
