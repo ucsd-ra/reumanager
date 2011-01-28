b67a276020129bf22488353e2ab8246aaafeefbd
jgrevich <jgrevich@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2011-01-28 06:09:55 +0000

    changed sender_address. removed exceptional config.

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,3 +1,3 @@
 ExceptionNotification::Notifier.exception_recipients = "jgrevich@ucsd.edu"
-ExceptionNotification::Notifier.sender_address = %("Rails Exception" <nsfreu@be.ucsd.edu>)
+ExceptionNotification::Notifier.sender_address = %("Rails Exception" <exception@be.ucsd.edu>)
 ExceptionNotification::Notifier.email_prefix = "[NSFREU] "
