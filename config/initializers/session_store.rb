4eac9ef23877f86e21efa85e2f03780a538b1e02
jgrevich <jgrevich@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2012-01-10 01:56:20 +0000

    almost forgot to make unique keys

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,7 +6,7 @@
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
   :key => '_nsfreu_session',
-  :secret      => '9e4b55560c0e8d28b3ca245987755dbf2d249b68a6cf18391cc3fe52d9754f8607be95f34095dfc5891df6ffe9f8d9d277bfb8d33ccffdeaf72ebcf9f736d289'
+  :secret      => '9e4b55560c0e8d28b3ca2459877bf2d24ff9b68a6cf183919754124dfsff8607be95f34095dfc5891df6ffe9f8d9d277bfb8d33ccffdeaf72ebcf9f736d289'
 }
 
 # Use the database for sessions instead of the cookie-based default,
