3d9a00bf94492bdddd0545be04a3c0b63b01330c
jgrevich <jgrevich@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2012-03-20 22:17:29 +0000

    added josh to admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,6 @@ u.save
 u.set_to_admin!
 
 # create admins
-u = User.new(:firstname => 'Eric', :lastname => 'Simms', :login => 'esimms@ucsd.edu', :email => 'esimms@ucsd.edu', :password => 'DemosdfdsfsApp', :password_confirmation => 'DemosdfdsfsApp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
+u = User.new(:firstname => 'Josh', :lastname => 'Reeves', :login => 'jdreevess@ucsd.edu', :email => 'jdreevess@ucsd.edu', :password => 'DemosdfdsfsApp', :password_confirmation => 'DemosdfdsfsApp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
 u.save
 u.set_to_admin!
-
