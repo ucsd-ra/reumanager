7e16c1fb5cb237f1f0592e6aa16465d494c59131
jgrevich <jgrevich@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2012-11-19 21:36:31 +0000

    change email to admin@reu.ucsd.edu

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # create admins
-u = User.new(:firstname => 'NSFREU', :lastname => 'Admin', :login => 'nsfreu@reu.ucsd.edu', :email => 'nsfreu@reu.ucsd.edu', :password => 'DemoApp', :password_confirmation => 'DemoApp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
+u = User.new(:firstname => 'NSFREU', :lastname => 'Admin', :login => 'admin@reu.ucsd.edu', :email => 'admin@reu.ucsd.edu', :password => 'DemoApp', :password_confirmation => 'DemoApp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
 u.save
 u.set_to_admin!
 
