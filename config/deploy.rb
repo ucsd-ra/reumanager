47bea07a6ae171ab1896f80f802ee6f7c1da0a7d
jgrevich <jgrevich@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2012-12-29 01:18:02 +0000

    remove cap-db from bundle. fix errors in deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,34 +2,36 @@ require "bundler/capistrano"
 require "rvm/capistrano"
 require "whenever/capistrano"
 
-default_run_options[:pty] = true
-
 set :application, "surf" #matches names used in smf_template.erb
-set :backup_path, "#{shared_path}/system"
-set :db_credentials_file, "db_credentials_file.yml"
-set :db_root_credentials_file, "root_db_credentials_file.yml"
-set :db_server_app, "mysql"
-set :db_database_name, 'reu_production'
-set :db_username, 'reu'
-set :deploy_to, "/var/www/#{application}" # I like this location
-set :domain, '192.168.126.147'
-set :keep_releases, 2
-set :passenger_port, 4060
-set :passenger_cmd,  "passenger"
-set :rails_env,      "production"
 set :repository,  "https://vishnu.ucsd.edu/svn/nsfreu/branches/surf"
-set :scm, :subversion
+set :domain, '192.168.126.147'
+set :deploy_to, "/var/www/#{application}" # I like this location
+set :user, "ubuntu"
+set :keep_releases, 2
 set :rvm_ruby_string, "ree@#{application}"
 set :rvm_type, :system
+set :scm, :subversion
 set :scm_passphrase, "5'utr $Saihung"
-set :user, "ubuntu"
-set :use_sudo, false
+
+default_run_options[:pty] = true
 
 role :app, domain
 role :web, domain
 role :db,  domain, :primary => true
 
 ## modified for passenger standalone
+set :rails_env,      "production"
+set :passenger_port, 4060
+set :passenger_cmd,  "passenger"
+
+# variables for cap-db
+set :backup_path, "#{shared_path}/system"
+set :db_credentials_file, "db_credentials_file.yml"
+set :db_root_credentials_file, "root_db_credentials_file.yml"
+set :db_server_app, "mysql"
+set :db_database_name, 'reu_production'
+set :db_username, 'reu'
+
 
 namespace :deploy do
   
@@ -48,7 +50,6 @@ namespace :deploy do
   end
 
   task :restart, :roles => :app, :except => { :no_release => true } do
-
     chown
     run <<-CMD
       if [[ -f #{current_path}/tmp/pids/passenger.#{passenger_port}.pid ]];
@@ -58,5 +59,4 @@ namespace :deploy do
     CMD
     run "cd #{current_path} && #{passenger_cmd} start -e #{rails_env} -p #{passenger_port} -d"
   end
-
 end
