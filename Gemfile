a07e436db69aa16c99814f5b6241b5202292f5c6
jgrevich <jgrevich@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2012-12-29 10:28:47 +0000

    updated rvmrc with appropriate syntax. refined gems, db, and deploy similar to other suburi apps.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'http://rubygems.org'
 source 'http://gems.github.com'
 
+gem 'capistrano'
+gem 'rvm-capistrano'
+
 gem 'rails', '2.3.14'
 gem 'carmen', '0.2.8'
 #gem 'exception_notification', '2.3.3.0'
@@ -13,7 +16,7 @@ gem 'rake', '0.9.2.2'
 gem 'rcov', '0.9.9'
 gem 'rdoc'
 gem 'redbox', '1.0.4'
-gem 'RedCloth', '4.2.7', :require => 'RedCloth'
+gem 'RedCloth', '4.2.9', :require => 'RedCloth'
 gem 'restful_authentication', '1.1.6', :require => 'restful_authentication'
 gem 'rvm'
 gem 'spreadsheet', '0.6.5.5', :require => 'spreadsheet'
@@ -23,9 +26,7 @@ gem 'will_paginate', '2.3.15', :require => 'will_paginate'
 gem 'yaml_db', '0.1.0'
 
 group :development do
-  gem 'capistrano'
 	gem 'sqlite3-ruby', :require => 'sqlite3'
-	gem 'ruby-debug'
 	gem 'wirble'
 end
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,16 @@ require "bundler/capistrano"
 require "rvm/capistrano"
 require "whenever/capistrano"
 
-set :application, "nsfreu" #matches names used in smf_template.erb
+set :application, "be" #matches names used in smf_template.erb
 set :repository,  "https://vishnu.ucsd.edu/svn/nsfreu/branches/ucsd_bioengineering"
-set :domain, 'vishnu.ucsd.edu'
-set :deploy_to, "/var/rails/#{application}" # I like this location
+set :domain, '192.168.126.147'
+set :deploy_to, "/var/www/#{application}" # I like this location
 set :user, "ubuntu"
 set :keep_releases, 2
 set :rvm_ruby_string, "ree@#{application}"
-set :server_name, "vishnu.ucsd.edu"
+set :rvm_type, :system
+set :scm, :subversion
+set :scm_passphrase, "5'utr $Saihung"
 
 default_run_options[:pty] = true
 
@@ -20,9 +22,26 @@ role :db,  domain, :primary => true
 ## modified for passenger standalone
 set :rails_env,      "production"
 set :passenger_port, 4010
-set :passenger_cmd,  "passenger"
+set :passenger_cmd,  "bundle exec passenger"
+set :whenever_command, "bundle exec whenever"
+
+# variables for cap-db
+set :backup_path, "#{shared_path}/system"
+set :db_credentials_file, "db_credentials_file.yml"
+set :db_root_credentials_file, "root_db_credentials_file.yml"
+set :db_server_app, "mysql"
+set :db_database_name, 'reu_demo_production'
+set :db_username, 'reu_be'
+
 
 namespace :deploy do
+  
+  desc "chown & chmod to www-data"
+  task :chown do
+    sudo "chown -R #{user}:www-data #{deploy_to}"
+    sudo "chmod -R 770 #{deploy_to}"
+  end
+  
   task :start, :roles => :app, :except => { :no_release => true } do
     run "cd #{current_path} && #{passenger_cmd} start -e #{rails_env} -p #{passenger_port} -d"
   end
@@ -32,6 +51,7 @@ namespace :deploy do
   end
 
   task :restart, :roles => :app, :except => { :no_release => true } do
+    chown
     run <<-CMD
       if [[ -f #{current_path}/tmp/pids/passenger.#{passenger_port}.pid ]];
       then
