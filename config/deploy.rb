cb42149403cbd606c23949a6fad29ffadcf9de6f
jgrevich <jgrevich@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2013-01-13 00:23:22 +0000

    update deploy and db.yml for indra.test.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,14 +4,14 @@ require "rvm/capistrano"
 set :whenever_command, "bundle exec whenever"
 require "whenever/capistrano"
 
-set :application, "nsfreudemo" #matches names used in smf_template.erb
+set :application, "demo" #matches names used in smf_template.erb
 set :repository,  "https://vishnu.ucsd.edu/svn/nsfreu/branches/nsfreu_demo"
-set :domain, 'vishnu.ucsd.edu'
-set :deploy_to, "/var/rails/#{application}" # I like this location
+set :domain, '192.168.10.103'
+set :deploy_to, "/var/www/#{application}" # I like this location
 set :user, "ubuntu"
 set :keep_releases, 2
 set :rvm_ruby_string, "ree@#{application}"
-set :rvm_type, :user
+set :rvm_type, :system
 set :scm, :subversion
 
 default_run_options[:pty] = true
