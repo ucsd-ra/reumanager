911212481c2951552d91de7e8f752e76c920fb51
jgrevich <jgrevich@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2013-02-15 21:08:01 +0000

    updated seeds for correct admins. found more settings that needed to be updated.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ u = User.new(:firstname => 'NSFREU', :lastname => 'Admin', :login => 'admin@reum
 u.save
 u.set_to_admin!
 # create admins
-u = User.new(:firstname => 'Ryan', :lastname => 'Kastner', :login => 'kastner@ucsd.edu', :email => 'kastner@ucsd.edu', :password => 'Change-Me-Please', :password_confirmation => 'Change-Me-Please', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
+u = User.new(:firstname => 'Melissa', :lastname => 'Micou', :login => 'mmicou@ucsd.edu', :email => 'mmicou@ucsd.edu', :password => 'Change-Me-Please', :password_confirmation => 'Change-Me-Please', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
 u.save
 u.set_to_admin!
 # create admins
