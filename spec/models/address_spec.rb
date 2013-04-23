fd328545a26f054b2f286511c5ee7f9598498101
Vivek Poola <vivekpoola@ares.san.rr.com>
2013-04-22 19:46:34 -0700

    finished more model specs

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Address do
+#  it "is valid with an address, address2, city, country, state, zip, label, permanent" do
+#    address = Address.new(city: 'San Diego', country: 'United States of America', state: 'CA', zip:'92092', label: 'yes', permanent: 'unsure')
+#    expect(address).to be_valid
+#  end
+  
+#  it "is invalid without a city" do
+#    expect(Address.new(city: nil)).to have(1).error_on(:city)
+#  end
+  
+#  it "is invalid without a zip" do
+#    expect(Address.new(zip: nil)).to have(1).error_on(:zip)
+#  end
+  
+#  it "is invalid without a label" do
+#    expect(Address.new(label: nil)).to have(1).error_on(:label)
+#  end
+  
+#  it "is invalid without a permanent" do
+#    expect(Address.new(permanent: nil)).to have(1).error_on(:permanent)
+#  end
   
 end
--- spec/models/award_spec.rb
+++ spec/models/award_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Award do
+  it "should have a date, description, and is valid with a title" do
+    award = Award.new(date: '4/24/1988', description: 'Some text goes in here', title: 'title of award')
+    expect(award).to be_valid
+  end
+  
+  it "is invalid without a title" do
+    expect(Award.new(title: nil)).to have(1).error_on(:title)
+  end
+  
   
 end
