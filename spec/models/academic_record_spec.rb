2d79a0a1e65bfabc1a6f132614a32214a168c2b3
Vivek Poola <vivekpoola@ares.san.rr.com>
2013-04-22 21:48:28 -0700

    refined model specs

--- spec/models/academic_record_spec.rb
+++ spec/models/academic_record_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe AcademicRecord do
-  it "is valid with a university, degree, start, finish, gpa, gpa_range" do
-    academicrecord = AcademicRecord.new(university: 'University of California, San Diego', degree: 'Biology', start: '01/01/2000', finish: '01/01/2004', gpa: '3.5', gpa_range: '4.0')
+
+  # association(s)
+  it { should belong_to :applicant }
+  #it { should belong_to :foreign_key }? Notice in academic_record.rb, there is no :class_name = > "applicant_id"
+  
+  it "should have an academic level, and is valid with a university, degree, start, finish, gpa, gpa_range" do
+    academicrecord = AcademicRecord.new(academic_level: 'Graduate', university: 'University of California, San Diego', degree: 'Biology', start: '01/01/2000', finish: '01/01/2004', gpa: '3.5', gpa_range: '4.0')
     expect(academicrecord).to be_valid
   end
-  
+
+  # validation tests
   it "is invalid without a university" do
     expect(AcademicRecord.new(university: nil)).to have(1).error_on(:university) 
   end
