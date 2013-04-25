cda1f6f85e2e8367eb953a7c754cb7578dcda330
Justin Grevich <justin@grevi.ch>
2013-04-25 16:55:50 -0700

    plain text links for plain text mails

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -10,20 +10,19 @@ class Notification < ActionMailer::Base
 
   def recommendation_request(recommendation)
     @recommendation = recommendation
-    @url = applicants_recommendations_edit_path(token: recommendation.token)
+    @url = 'https://' + Rails.configuration.action_mailer[:default_url_options][:host] + applicants_recommendations_edit_path(token: recommendation.token)
     mail(:to => recommendation.recommender.email, :subject => "REU recommendation request for #{recommendation.applicant.name}")
   end
   
   def recommendation_follow_up_request(recommendation)
     @recommendation = recommendation
-    @url = applicants_recommendations_edit_path(token: recommendation.token)
+    @url = Rails.configuration.action_mailer[:default_url_options][:host] + applicants_recommendations_edit_path(token: recommendation.token)
     mail(:to => recommendation.recommender.email, :subject => "REU follow-up recommendation request for #{recommendation.applicant.name}")
   end  
 
   # thank you to recommender once recommendation is received.
   def recommendation_thanks(recommendation)
     @recommendation = recommendation
-    @url = applicants_recommendations_edit_path(token: recommendation.token)
     mail(:to => recommendation.recommender.email, :subject => "REU recommendation received for #{recommendation.applicant.name}")
   end  
   
