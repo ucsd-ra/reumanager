81c0ad74745c079d28505f7979815443e8067166
jgrevich <jgrevich@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2014-01-01 18:57:14 +0000

    2014 updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,16 +3,15 @@ require "rvm/capistrano"
 
 #set :application, "reu_surf" #matches names used in smf_template.erb
 set :application, "surf" #matches names used in smf_template.erb
-set :repository,  "https://vishnu.ucsd.edu/svn/nsfreu/branches/surf"
+set :repository,  "https://iem.ucsd.edu/svn/nsfreu/branches/surf"
 #set :domain, "192.168.10.103"
-set :domain, "vishnu.ucsd.edu"
+set :domain, "indra.ucsd.edu"
 #set :deploy_to, "/var/www/#{application}" # I like this location
-set :deploy_to, "/var/rails/#{application}" # I like this location
+set :deploy_to, "/var/www/#{application}" # I like this location
 set :user, "ubuntu"
-set :keep_releases, 2
+set :keep_releases, 3
 set :rvm_ruby_string, "ree@#{application}"
-#set :rvm_type, :system
-set :rvm_type, :user
+set :rvm_type, :system
 set :scm, :subversion
 
 default_run_options[:pty] = true
@@ -23,18 +22,17 @@ role :db,  domain, :primary => true
 
 ## modified for passenger standalone
 set :rails_env,      "production"
-#set :passenger_port, 4020
-set :passenger_port, 4060
-set :passenger_cmd,  "bundle exec passenger"
+set :thin_port, 4020
+set :thin_cmd,  "bundle exec thin"
+set :whenever_command, "bundle exec whenever"
 
 # variables for cap-db
 set :backup_path, "#{shared_path}/system"
 set :db_credentials_file, "db_credentials_file.yml"
 set :db_root_credentials_file, "root_db_credentials_file.yml"
 set :db_server_app, "mysql"
-set :db_database_name, 'reu_production'
-set :db_username, 'reu'
-
+set :db_database_name, 'surf_production'
+set :db_username, 'surf'
 
 namespace :deploy do
   
@@ -45,21 +43,15 @@ namespace :deploy do
   end
   
   task :start, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && #{passenger_cmd} start -e #{rails_env} -p #{passenger_port} -d"
+    run "cd #{current_path} && #{thin_cmd} start -e #{rails_env} -p #{thin_port} -d"
   end
 
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && #{passenger_cmd} stop -p #{passenger_port}"
+    run "cd #{current_path} && #{thin_cmd} stop -p #{thin_port}"
   end
 
   task :restart, :roles => :app, :except => { :no_release => true } do
     chown
-    run <<-CMD
-      if [[ -f #{current_path}/tmp/pids/passenger.#{passenger_port}.pid ]];
-      then
-        cd #{current_path} && #{passenger_cmd} stop -p #{passenger_port};
-      fi
-    CMD
-    run "cd #{current_path} && #{passenger_cmd} start -e #{rails_env} -p #{passenger_port} -d"
+    run "cd #{current_path} && #{thin_cmd} restart"
   end
 end
