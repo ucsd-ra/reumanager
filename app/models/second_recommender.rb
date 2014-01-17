592290e64ccf42242889ca9fced9030f5f28c5e4
jgrevich <jgrevich@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2014-01-17 08:51:38 +0000

    add second recommender

---
+++ app/models/second_recommender.rb
@@ -0,0 +1,7 @@
+class Recommender < ActiveRecord::Base
+  belongs_to  :user
+  has_one                     :recommendation
+  validates_presence_of       :name, :title, :department, :college, :phone, :email
+  validates_format_of         :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+  validates_inclusion_of      :waive_rights, :in => [true, false], :on => :create, :message => "must be selected as 'do' or 'do not'"
+end
