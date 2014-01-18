69342aa5258f06fb1b9a5fd172bcc9bcb7ae1b52
jgrevich <jgrevich@8a277a05-2170-43bf-da54-e2abe9d0a9f3>
2014-01-18 01:06:13 +0000

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,24 @@
 # create admins
-u = User.new(:firstname => 'SURF REU', :lastname => 'Admin', :login => 'jgrevich@ucsd.edu', :email => 'jgrevich@ucsd.edu', :password => 'DemfsdfdsoApp', :password_confirmation => 'DemfsdfdsoApp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
+u = User.new(:firstname => 'SURF REU', :lastname => 'Admin', :login => 'jgrevich@ucsd.edu', :email => 'jgrevich@ucsd.edu', :password => 'DemfsdfdsoApp', :password_confirmation => 'DemfsdfdsoApp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947", :father_edu => 'none', :mother_edu => 'none')
 u.save
 u.set_to_admin!
 
 # create admins
-u = User.new(:firstname => 'Meliisa', :lastname => 'Miccou', :login => 'mmicou@ucsd.edu', :email => 'mmicou@ucsd.edu', :password => 'jil-ye-fly-gre', :password_confirmation => 'jil-ye-fly-gre', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
+u = User.new(:firstname => 'Meliisa', :lastname => 'Miccou', :login => 'mmicou@ucsd.edu', :email => 'mmicou@ucsd.edu', :password => 'jil-ye-fly-gre', :password_confirmation => 'jil-ye-fly-gre', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947", :father_edu => 'none', :mother_edu => 'none')
 u.save
 u.set_to_admin!
 
 # create admins
-u = User.new(:firstname => 'Megan', :lastname => 'Posner', :login => 'mposner@ucsd.edu', :email => 'mposner@ucsd.edu', :password => 'ep-co-hiv-ep-o', :password_confirmation => 'ep-co-hiv-ep-o', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
+u = User.new(:firstname => 'Megan', :lastname => 'Posner', :login => 'mposner@ucsd.edu', :email => 'mposner@ucsd.edu', :password => 'ep-co-hiv-ep-o', :password_confirmation => 'ep-co-hiv-ep-o', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947", :father_edu => 'none', :mother_edu => 'none')
 u.save
 u.set_to_admin!
 
 # create admins
-u = User.new(:firstname => 'Robert', :lastname => 'Ross', :login => 'rross@ucsd.edu', :email => 'rross@ucsd.edu', :password => 'hyasog-ik-be', :password_confirmation => 'hyasog-ik-be', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
+u = User.new(:firstname => 'Robert', :lastname => 'Ross', :login => 'rross@ucsd.edu', :email => 'rross@ucsd.edu', :password => 'hyasog-ik-be', :password_confirmation => 'hyasog-ik-be', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947", :father_edu => 'none', :mother_edu => 'none')
 u.save
 u.set_to_admin!
 
 # create admins
-u = User.new(:firstname => 'Mary', :lastname => 'Kiisel', :login => 'mkiisel@ucsd.edu', :email => 'mkiisel@ucsd.edu', :password => 'ik-be-sdfp', :password_confirmation => 'ik-be-sdfp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
+u = User.new(:firstname => 'Mary', :lastname => 'Kiisel', :login => 'mkiisel@ucsd.edu', :email => 'mkiisel@ucsd.edu', :password => 'ik-be-sdfp', :password_confirmation => 'ik-be-sdfp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947", :father_edu => 'none', :mother_edu => 'none')
 u.save
 u.set_to_admin!
