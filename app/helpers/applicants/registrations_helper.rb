10ed03e4baf4641247dfa1f4027e8b4673d89a60
Rob Kaufman <rob@notch8.com>
2014-12-01 20:11:26 -0800

    fix admin links, fix recommendation status

--- app/helpers/applicants/registrations_helper.rb
+++ app/helpers/applicants/registrations_helper.rb
@@ -4,7 +4,7 @@ module Applicants::RegistrationsHelper
   end
 
   def recommendation_received_label(recommender)
-    recommendation = current_applicant.where(:recommender_id => recommender).first
+    recommendation = current_applicant.recommendations.where(:recommender_id => recommender.id).first
     raw recommendation.valid?  ? " <span class='label label-success'>[RECIEVED]</span>" : " <span class='label label-info'>[NOT RECIEVED]</span> &nbsp;&nbsp;&nbsp; #{ link_to 'Resend Request', applicants_recommendations_request_path(recommender), class: 'btn btn-mini', method: :post}" if current_applicant.submitted?
   end
 
