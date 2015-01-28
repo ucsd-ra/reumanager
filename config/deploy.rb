8ada0822ca4eb8ef2bf3a2ab91924edfbf465e41
Rob Kaufman <rob@notch8.com>
2015-01-27 21:58:04 -0800

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,16 +3,16 @@ require "rvm/capistrano"
 
 #set :application, "reu_surf" #matches names used in smf_template.erb
 set :application, "surf" #matches names used in smf_template.erb
-set :repository,  "https://iem.ucsd.edu/svn/nsfreu/branches/surf"
-#set :domain, "192.168.10.103"
+set :repository,  "https://github.com/ucsd-ra/reumanager.git"
+set :branch, :rqi
 set :domain, "indra.ucsd.edu"
-#set :deploy_to, "/var/www/#{application}" # I like this location
 set :deploy_to, "/var/www/#{application}" # I like this location
+set :deploy_via, :remote_cache
 set :user, "ubuntu"
 set :keep_releases, 3
 set :rvm_ruby_string, "ree@#{application}"
 set :rvm_type, :system
-set :scm, :subversion
+set :scm, :git
 
 default_run_options[:pty] = true
 
@@ -35,13 +35,13 @@ set :db_database_name, 'surf_production'
 set :db_username, 'surf'
 
 namespace :deploy do
-  
+
   desc "chown & chmod to www-data"
   task :chown do
     sudo "chown -R #{user}:www-data #{deploy_to}"
     sudo "chmod -R 770 #{deploy_to}"
   end
-  
+
   task :start, :roles => :app, :except => { :no_release => true } do
     run "cd #{current_path} && #{thin_cmd} start -e #{rails_env} -p #{thin_port} -d"
   end
