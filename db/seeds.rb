c004133844256c90a88d97af7db7722d316126df
Rob Kaufman <rob@notch8.com>
2015-01-28 18:46:23 -0800

    fix seed typo, change enrolled from again

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,15 @@ u.set_to_admin!
 
 
 # create admins
-u = User.new(:firstname => 'Jane', :lastname => 'Teranes', :login => 'jteranes@ucsd.edu', :email => 'jteranes@ucsd.edu', :password => 'DemosdfdsfsApp', :password_confirmation => 'DemosdfdsfsApp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
+u = User.new(:firstname => 'Jane', :lastname => 'Teranes', :login => 'jteranes@ucsd.edu', :email => 'jteranes@ucsd.edu', :password => 'DemfsdfdsoApp', :password_confirmation => 'DemfsdfdsoApp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
 u.save
 u.set_to_admin!
 
 # create admins
-u = User.new(:firstname => 'Josh', :lastname => 'Reeves', :login => 'jdreevess@ucsd.edu', :email => 'jdreevess@ucsd.edu', :password => 'DemosdfdsfsApp', :password_confirmation => 'DemosdfdsfsApp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
+u = User.new(:firstname => 'Josh', :lastname => 'Reeves', :login => 'jdreeves@ucsd.edu', :email => 'jdreeves@ucsd.edu', :password => 'DemfsdfdsoApp', :password_confirmation => 'DemfsdfdsoApp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
 u.save
 u.set_to_admin!
 
-u = User.new(:firstname => 'J3', :lastname => 'Taylor', :login => 'j3taylor@ucsd.edu', :email => 'j3taylor@ucsd.edu', :password => 'DemosdfdsfsApp', :password_confirmation => 'DemosdfdsfsApp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
+u = User.new(:firstname => 'J3', :lastname => 'Taylor', :login => 'j3taylor@ucsd.edu', :email => 'j3taylor@ucsd.edu', :password => 'DemfsdfdsoApp', :password_confirmation => 'DemfsdfdsoApp', :role_id => 1, :dob => Time.now - 30.years, :street => '9500 Gilman Drive #0412', :city => 'La Jolla', :state => "CA", :zip => "92093-0412", :citizenship => "United States", :phone => "858-822-5947")
 u.save
 u.set_to_admin!
