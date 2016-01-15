96b5736cd1c4932d34cfa276b2f66abbcec5c1e7
Robert Dennett <r.dennett3@gmail.com>
2016-01-14 20:09:33 -0500

    styling updated for landing, tour, and pricing pages

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
+<<<<<<< Updated upstream
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20121104040557) do
@@ -23,6 +24,29 @@ ActiveRecord::Schema.define(:version => 20121104040557) do
     t.string   "image_ext"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+=======
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20141126234350) do
+
+  create_table "academic_records", force: true do |t|
+    t.string   "university"
+    t.date     "start"
+    t.date     "finish"
+    t.string   "degree"
+    t.float    "gpa",                     limit: 24
+    t.float    "gpa_range",               limit: 24, default: 4.0
+    t.text     "gpa_comment"
+    t.integer  "applicant_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "transcript_file_name"
+    t.string   "transcript_content_type"
+    t.integer  "transcript_file_size"
+    t.datetime "transcript_updated_at"
+    t.string   "major"
+    t.string   "minor"
+>>>>>>> Stashed changes
   end
 
   create_table "refinery_page_part_translations", :force => true do |t|
@@ -33,12 +57,21 @@ ActiveRecord::Schema.define(:version => 20121104040557) do
     t.datetime "updated_at",            :null => false
   end
 
+<<<<<<< Updated upstream
   add_index "refinery_page_part_translations", ["locale"], :name => "index_refinery_page_part_translations_on_locale"
   add_index "refinery_page_part_translations", ["refinery_page_part_id"], :name => "index_f9716c4215584edbca2557e32706a5ae084a15ef"
+=======
+  add_index "applicants", ["authentication_token"], name: "index_applicants_on_authentication_token", unique: true, using: :btree
+  add_index "applicants", ["confirmation_token"], name: "index_applicants_on_confirmation_token", unique: true, using: :btree
+  add_index "applicants", ["email"], name: "index_applicants_on_email", unique: true, using: :btree
+  add_index "applicants", ["reset_password_token"], name: "index_applicants_on_reset_password_token", unique: true, using: :btree
+  add_index "applicants", ["unlock_token"], name: "index_applicants_on_unlock_token", unique: true, using: :btree
+>>>>>>> Stashed changes
 
   create_table "refinery_page_parts", :force => true do |t|
     t.integer  "refinery_page_id"
     t.string   "title"
+<<<<<<< Updated upstream
     t.text     "body"
     t.integer  "position"
     t.datetime "created_at",       :null => false
@@ -47,6 +80,27 @@ ActiveRecord::Schema.define(:version => 20121104040557) do
 
   add_index "refinery_page_parts", ["id"], :name => "index_refinery_page_parts_on_id"
   add_index "refinery_page_parts", ["refinery_page_id"], :name => "index_refinery_page_parts_on_refinery_page_id"
+=======
+    t.date     "date"
+    t.text     "description"
+    t.integer  "applicant_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "rails_admin_histories", force: true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      limit: 2
+    t.integer  "year",       limit: 8
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories", using: :btree
+>>>>>>> Stashed changes
 
   create_table "refinery_page_translations", :force => true do |t|
     t.integer  "refinery_page_id"
@@ -59,6 +113,7 @@ ActiveRecord::Schema.define(:version => 20121104040557) do
     t.datetime "updated_at",       :null => false
   end
 
+<<<<<<< Updated upstream
   add_index "refinery_page_translations", ["locale"], :name => "index_refinery_page_translations_on_locale"
   add_index "refinery_page_translations", ["refinery_page_id"], :name => "index_d079468f88bff1c6ea81573a0d019ba8bf5c2902"
 
@@ -80,6 +135,10 @@ ActiveRecord::Schema.define(:version => 20121104040557) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
   end
+=======
+  add_index "recommendations", ["applicant_id"], name: "index_recommendations_on_applicant_id", using: :btree
+  add_index "recommendations", ["recommender_id"], name: "index_recommendations_on_recommender_id", using: :btree
+>>>>>>> Stashed changes
 
   add_index "refinery_pages", ["depth"], :name => "index_refinery_pages_on_depth"
   add_index "refinery_pages", ["id"], :name => "index_refinery_pages_on_id"
@@ -106,8 +165,12 @@ ActiveRecord::Schema.define(:version => 20121104040557) do
     t.integer "role_id"
   end
 
+<<<<<<< Updated upstream
   add_index "refinery_roles_users", ["role_id", "user_id"], :name => "index_refinery_roles_users_on_role_id_and_user_id"
   add_index "refinery_roles_users", ["user_id", "role_id"], :name => "index_refinery_roles_users_on_user_id_and_role_id"
+=======
+  add_index "settings", ["name"], name: "index_settings_on_name", using: :btree
+>>>>>>> Stashed changes
 
   create_table "refinery_user_plugins", :force => true do |t|
     t.integer "user_id"
@@ -146,7 +209,15 @@ ActiveRecord::Schema.define(:version => 20121104040557) do
     t.datetime "updated_at",       :null => false
   end
 
+<<<<<<< Updated upstream
   add_index "seo_meta", ["id"], :name => "index_seo_meta_on_id"
   add_index "seo_meta", ["seo_meta_id", "seo_meta_type"], :name => "index_seo_meta_on_seo_meta_id_and_seo_meta_type"
+=======
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
+>>>>>>> Stashed changes
 
 end
